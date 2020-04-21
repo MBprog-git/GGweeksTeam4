3e1e7d10f7ee83d996823ed75194f423a39f96ab
sangshu <54836318+sangshu-sang@users.noreply.github.com>
2020-04-21 14:46:45 +0200

    Proto Fps

---
+++ GgweekT4/Assets/Script/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Déplacement")]
+    public float speed;
+public float Gravity;
+public float FallSpeedMax;
+    [Space]
+    [Header("Caméra")]
+    public float SensitivityH;
+    public float SensitivityV;
+    public float OffsetX;
+    public float OffsetY;
+    public float OffsetZ;
+
+    public float LimiteCamDown = -90;
+    public float LimiteCamUp = 15;
+
+    [Space]
+    [Header("Manager")]
+
+    public static GameManager instance;
+    public Camera Cam;
+    public GameObject Player;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Camera
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        yaw += SensitivityH * Input.GetAxis("Mouse X");
+        pitch -= SensitivityV * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, LimiteCamDown, LimiteCamUp);
+        Cam.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        Player.transform.eulerAngles = new Vector3(Player.transform.eulerAngles.x, yaw, Player.transform.eulerAngles.z);
+        Cam.transform.position = new Vector3(Player.transform.position.x + OffsetX, Player.transform.position.y + OffsetY, Player.transform.position.z + OffsetZ);
+    }
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+    public void MyLoadScene(string nameScene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nameScene);
+     
+    }
+}
---
+++ GgweekT4/Assets/Script/PlayerController.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+
+    bool Avant;
+    bool Reculer;
+    bool Droite;
+    bool Gauche;
+ 
+
+
+     float speed;
+
+  
+    bool _Grounded;
+    float Gravity;
+    float SpeedFall;
+    float SpeedFallMax;
+
+    public Transform Respawn;
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = GameManager.instance.speed;
+        Gravity = GameManager.instance.Gravity;
+        SpeedFallMax = GameManager.instance.FallSpeedMax;
+     
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+          //Move Velocity
+          if (Avant)
+          {
+            transform.position += transform.forward * Time.deltaTime * speed;
+           
+          }
+          if (Reculer)
+          {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+         
+          }
+          if (Droite)
+          {
+            transform.position += transform.right * Time.deltaTime * speed;
+           
+          }
+          if (Gauche)
+          {
+              
+            transform.position -= transform.right * Time.deltaTime * speed;
+          }
+
+        Gravitation(Time.fixedDeltaTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Inputs Move
+
+
+       
+
+            if (Input.GetKey(KeyCode.Z))
+            {
+                Avant = true;
+            }
+            else
+            {
+                Avant = false;
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                Gauche = true;
+            }
+            else
+            {
+                Gauche = false;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                Reculer = true;
+            }
+            else
+            {
+                Reculer = false;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                Droite = true;
+            }
+            else
+            {
+                Droite = false;
+            }
+        
+
+
+    }
+
+    private void Gravitation(float DeltaTime)
+    {
+        if (_Grounded)
+        {
+            SpeedFall = 0;
+        }
+        else
+        {
+            SpeedFall -= Gravity * DeltaTime;
+            if (SpeedFall < -SpeedFallMax)
+            {
+                SpeedFall = -SpeedFallMax;
+            
+            }
+            rb.velocity = new Vector3(rb.velocity.x, SpeedFall, rb.velocity.z);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Respawn")
+        {
+            transform.position = Respawn.transform.position;
+        }
+        if(collision.gameObject.tag == "Scene")
+        {
+            _Grounded = true;
+        }
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Scene")
+        {
+            _Grounded = false;
+        }
+        
+    }
+
+
+}
+
+      
+
