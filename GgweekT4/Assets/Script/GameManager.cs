8562ae7e4639ddf9cc146ff25c2b845ce8c023cb
sangshu <54836318+sangshu-sang@users.noreply.github.com>
2020-04-21 16:31:03 +0200

    Raycast de ramassage objet

--- GgweekT4/Assets/Script/GameManager.cs
+++ GgweekT4/Assets/Script/GameManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [Header("Déplacement")]
+    
     public float speed;
-public float Gravity;
-public float FallSpeedMax;
+    public float Gravity;
+    public float FallSpeedMax;
+
     [Space]
     [Header("Caméra")]
     public float SensitivityH;
@@ -20,13 +22,19 @@ public float FallSpeedMax;
     public float LimiteCamUp = 15;
 
     [Space]
+   public  float _MaxDistance;
+    
+    [Space]
     [Header("Manager")]
 
-    public static GameManager instance;
     public Camera Cam;
     public GameObject Player;
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+    public GameObject RamasseFeedback;
+    public static GameManager instance;
+
+
 
     void Awake()
     {
@@ -52,6 +60,37 @@ public float FallSpeedMax;
         Cam.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
         Player.transform.eulerAngles = new Vector3(Player.transform.eulerAngles.x, yaw, Player.transform.eulerAngles.z);
         Cam.transform.position = new Vector3(Player.transform.position.x + OffsetX, Player.transform.position.y + OffsetY, Player.transform.position.z + OffsetZ);
+
+        //Sélection & Interaction
+            Vector3 Mouspos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(Mouspos, Cam.transform.TransformDirection(Vector3.forward), _MaxDistance);
+        Debug.DrawLine(Mouspos, Cam.transform.TransformDirection(Vector3.forward) * _MaxDistance, Color.red);
+        bool ObjetPresent = false;
+        foreach (RaycastHit hit in hits)
+        {
+            if  ( hit.collider.GetComponent<ItemTest>()!= null )
+            {
+                ObjetPresent = true;
+            }
+        }
+            if (ObjetPresent)
+            {
+                RamasseFeedback.SetActive(true);
+
+            }
+            else
+            {
+                RamasseFeedback.SetActive(false);
+
+            }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                //if(hit.)
+            }
+        }
     }
     public void doExitGame()
     {
