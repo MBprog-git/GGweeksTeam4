ceee9758e53c1ed5349b0e8d7d6c8c6df50a87da
sangshu <54836318+sangshu-sang@users.noreply.github.com>
2020-04-22 18:48:30 +0200

    Patch qte

--- GgweekT4/Assets/Script/QTE.cs
+++ GgweekT4/Assets/Script/QTE.cs
@@ -23,10 +23,7 @@ public class QTE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            StartQTE();
-        }
+  
 
         if (EnQTE)
         {
@@ -69,6 +66,10 @@ public class QTE : MonoBehaviour
     }
     public void StartQTE()
     {
+        if (EnQTE)
+        {
+            return;
+        }
         Indic.gameObject.SetActive(true);
         Indic.text = "";
         step = 0;
