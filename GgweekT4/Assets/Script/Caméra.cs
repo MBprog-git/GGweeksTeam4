a5e37cf41b364876834e8b6569353ed13d2c46ad
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-04-22 16:18:11 +0200

    ModifCamera

---
+++ GgweekT4/Assets/Script/Caméra.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caméra : MonoBehaviour
+{
+    public FxPro SuperVision;
+    public FxPro Etourdit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SuperVision.enabled = false;
+        Etourdit.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            SuperVision.enabled = !SuperVision.isActiveAndEnabled;
+        }
+        
+    }
+
+    public void etourdis() 
+    {
+        Etourdit.enabled = !Etourdit.isActiveAndEnabled;
+
+
+    }
+}
